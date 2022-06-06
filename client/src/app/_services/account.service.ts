f1726963963ca0225029865786ab5df83cdfdd42
prakaski <kiran.prakash@sunquestinfo.com>
2022-06-06 19:37:31 +0530

    Hooked register component with register service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -29,4 +29,15 @@ export class AccountService {
     localStorage.removeItem('user');
     this.currentUserSource.next(undefined);
   }
+  register(model: any){
+    return this.http.post<User>(this.baseUrl + '/account/register', model).pipe(
+      map((user) => {
+        if(user){
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        return user;
+      })
+    )
+  }
 }
