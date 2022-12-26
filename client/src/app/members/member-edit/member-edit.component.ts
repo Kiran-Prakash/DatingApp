17e54105255845e218ef0ee3a3b456de6eeea30f
prakaski <kiran.prakash@sunquestinfo.com>
2022-12-26 23:03:24 +0530

    Enhancement

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { User } from 'src/app/_models/user';
+import { take } from 'rxjs/operators';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+
+  member:Member | undefined;
+  user:User | null = null;
+  constructor(private accountService: AccountService, private memberService: MembersService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next:user => this.user = user
+    })
+   }
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember(){
+    if(!this.user) return;
+    this.memberService.getMember(this.user.username).pipe(take(1)).subscribe({
+      next:member => this.member = member
+    })
+  }
+
+}
