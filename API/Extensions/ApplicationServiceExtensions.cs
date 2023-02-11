a6bef049a6786085e7dfcfc33e9aa512239b56a9
prakaski <kiran.prakash@sunquestinfo.com>
2023-02-11 21:52:25 +0530

    Angular pre-update

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -17,6 +17,7 @@ namespace API.Extensions
             services.AddDbContext<DataContext>(options =>{
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
+            services.Configure<CloudinarySettings>(config.GetSection("CloudSettings"));
             return services;
         }
     }
